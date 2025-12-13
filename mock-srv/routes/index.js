fd19aeab1c44047a443e2946a499a6c5456f1652
Erick Burgos <eburgosrivas1997@gmail.com>
2025-12-13 17:52:10 -0600

    feat: test mocking data with fastify

---
+++ mock-srv/routes/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+
+module.exports = async function (fastify, opts) {
+  fastify.get('/', async function (request, reply) {
+    return [
+      {id: 'A1', name: 'Vacuum Cleaner', rrp: '99.99', info: 'The worst vacuum in the world.'},
+      {id: 'A2', name: 'Leaf Blower', rrp: '303.33', info: 'This product will blow your socks off.'},
+      {id: 'B1', name: 'Chocolate Bar', rrp: '22.40', info: 'Delicious overpriced chocolate.'}
+    ]
+  })
+}
